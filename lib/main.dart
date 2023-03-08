fc33c4e45f6af955c673ea3c306b3d1601cc769e
furkanksl <furkankoseoglu65@gmail.com>
2023-03-09 02:45:34 +0300

    Update main

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:battery_status/features/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Battery Status',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
